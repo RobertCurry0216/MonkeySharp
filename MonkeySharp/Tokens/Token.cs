ed0439be7f1f3ff705e736d2c81435b34274963b
Robert <robertcurry0216@gmail.com>
2021-03-23 21:46:30 +1100

    Basic token set up

---
+++ MonkeySharp/Tokens/Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeySharp.Tokens
+{
+    public class Token
+    {
+        public TokenType Type { get; set; }
+        public string Literal { get; set; }
+    }
+}
---
+++ MonkeySharp/Tokens/TokenType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeySharp.Tokens
+{
+    public enum TokenType
+    {
+        Illegal,
+        Eof,
+
+        //identifiers and literals
+        Ident,
+
+        Int,
+
+        // operators
+        Assign,
+
+        Plus,
+
+        // delimiters
+        Comma,
+
+        Semicolon,
+
+        LParen,
+        RParen,
+        LBrace,
+        RBrace,
+
+        // keywords
+        Function,
+
+        Let,
+    }
+}
